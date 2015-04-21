c6005f7c14cce85d45b924d584581195c2578807
nhan92 <nhannt92@gmail.com>
2015-04-21 10:30:15 +0700

    init tableview

--- tarotvn/ViewController.swift
+++ tarotvn/ViewController.swift
@@ -8,18 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var mang:[String]!
+    @IBOutlet weak var myListCard: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        mang = ["v","m","n"]
+       // self.myListCard.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        myListCard.dataSource = self
+        myListCard.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    
+    // MARK: - Table view data source
+    
+     func numberOfSectionsInTableView(myListCard: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+    
+     func tableView(myListCard: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return mang.count
+    }
+    
+    
+     func tableView(myListCard: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = myListCard.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! UITableViewCell
+        
+        // Configure the cell...
+        cell.textLabel?.text = mang[indexPath.row]
+        
+        return cell
+    }
+    
 
 }
 
