e7783131ed4eb9a1c52a75eb3e8b77fd1e64596e
nhan92 <nhannt92@gmail.com>
2015-06-16 10:54:46 +0700

    Added some file

---
+++ tarotvn/DetailCardViewController.swift
@@ -0,0 +1,58 @@
+//
+//  DetailCardViewController.swift
+//  tarotvn
+//
+//  Created by Nhan Nguyen on 5/20/15.
+//  Copyright (c) 2015 Nhan Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class DetailCardViewController: UIViewController {
+    
+     var arrCard:[TarotCard]!
+     var card:TarotCard!
+    
+    @IBOutlet weak var myDescription: UITextView!
+    @IBOutlet weak var mySegment: UISegmentedControl!
+
+    @IBAction func mySelection(sender: UISegmentedControl) {
+        
+        switch mySegment.selectedSegmentIndex
+        {
+        case 0:
+            myDescription.text = card.keyWord
+        case 1:
+            myDescription.text = card.forwardCard
+        case 2:
+            myDescription.text = card.information
+        case 3:
+            myDescription.text = card.keyDetail
+        default:
+            break; 
+        }
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
